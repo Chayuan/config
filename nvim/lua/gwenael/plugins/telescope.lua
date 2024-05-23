28d03027191896b1fddcab9e1960fec3e072c83f
chayuan <gwenael.rob@gmail.com>
2024-05-23 16:11:49 +0200

    feat: remove trouble for telescope as I do not use it for now

--- nvim/lua/gwenael/plugins/telescope.lua
+++ nvim/lua/gwenael/plugins/telescope.lua
@@ -1,41 +1,39 @@
-return  {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim",  build = "make" },
-    "nvim-tree/nvim-web-devicons",
-    "folke/todo-comments.nvim",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    local transform_mod = require("telescope.actions.mt").transform_mod
+return {
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+		"folke/todo-comments.nvim",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		local transform_mod = require("telescope.actions.mt").transform_mod
 
+		-- local trouble = require("trouble")
+		-- local trouble_telescope = require("trouble.providers.telescope")
 
-    local trouble = require("trouble")
-    local trouble_telescope = require("trouble.providers.telescope")
+		-- or create your custom action
+		local custom_actions = transform_mod({
+			open_trouble_qflist = function(prompt_bufnr)
+				trouble.toggle("quickfix")
+			end,
+		})
 
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+					},
+				},
+			},
+		})
 
-    -- or create your custom action
-    local custom_actions = transform_mod({
-      open_trouble_qflist = function(prompt_bufnr)
-        trouble.toggle("quickfix")
-      end,
-    })
-
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-          },
-        },
-      },
-    })
-
-    telescope.load_extensions("fzf")
-  end,
+		telescope.load_extensions("fzf")
+	end,
 }
