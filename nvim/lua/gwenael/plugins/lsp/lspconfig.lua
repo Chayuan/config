2195b9951ed7eff5efeb1006d46ffdf92efb7eab
chayuan <gwenael.rob@gmail.com>
2024-05-28 09:05:33 +0200

    feat: update lsp config

--- nvim/lua/gwenael/plugins/lsp/lspconfig.lua
+++ nvim/lua/gwenael/plugins/lsp/lspconfig.lua
@@ -1,77 +1,75 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-     "hrsh7th/cmp-nvim-lsp",
-    { "antosha417/nvim-lsp-file-operations", config = true },
-  },
-  config = function()
-    local lspconfig = require("lspconfig")
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		{ "antosha417/nvim-lsp-file-operations", config = true },
+	},
+	config = function()
+		local lspconfig = require("lspconfig")
 
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-    local keymap = vim.keymap
-   
-    vim.api.nvim_create_autocmd("LspAttach", {
-      callback = function(ev)
-        local opts = { silent = true, buffer = ev.buf }
-        keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
-        keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        keymap.set("n", "gr", require('telescope.builtin').lsp_references, opts)
-        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-      end,
-    })
+		local keymap = vim.keymap
 
-    -- used to enable autocompletion
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(ev)
+				local opts = { silent = true, buffer = ev.buf }
+				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+				keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+				keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
+				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+			end,
+		})
 
-    lspconfig["html"].setup({
-      capabilities = capabilities,
-    })
+		-- used to enable autocompletion
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    -- configure css server
-    lspconfig["cssls"].setup({
-      capabilities = capabilities,
-    })
+		lspconfig["html"].setup({
+			capabilities = capabilities,
+		})
 
-    -- Rust
-    lspconfig["rust_analyzer"].setup({
-      flags = flags,
-      capabilities = capabilities,
-      settings = {
-          ['rust-analyzer'] = {
-              cargo = {
-                  allFeatures = true,
-              },
-              checkOnSave = {
-                  allFeatures = true,
-                  command = 'clippy',
-              },
-              procMacro = {
-                  ignored = {
-                      ['async-trait'] = { 'async_trait' },
-                      ['napi-derive'] = { 'napi' },
-                      ['async-recursion'] = { 'async_recursion' },
-                  },
-              },
-          },
-      },
-    })
+		-- configure css server
+		lspconfig["cssls"].setup({
+			capabilities = capabilities,
+		})
 
-    -- configure tailwindcss server
-    lspconfig["tailwindcss"].setup({
-      capabilities = capabilities,
-    }) 
+		-- Rust
+		lspconfig["rust_analyzer"].setup({
+			flags = flags,
+			capabilities = capabilities,
+			settings = {
+				["rust-analyzer"] = {
+					cargo = {
+						allFeatures = true,
+					},
+					checkOnSave = {
+						allFeatures = true,
+						command = "clippy",
+					},
+					procMacro = {
+						ignored = {
+							["async-trait"] = { "async_trait" },
+							["napi-derive"] = { "napi" },
+							["async-recursion"] = { "async_recursion" },
+						},
+					},
+				},
+			},
+		})
 
-    -- configure typescript server with plugin
-    lspconfig["tsserver"].setup({
-      capabilities = capabilities,
-    })
-  end,
+		-- configure tailwindcss server
+		lspconfig["tailwindcss"].setup({
+			capabilities = capabilities,
+		})
+
+		-- configure typescript server with plugin
+		lspconfig["tsserver"].setup({
+			capabilities = capabilities,
+		})
+	end,
 }
-
-
