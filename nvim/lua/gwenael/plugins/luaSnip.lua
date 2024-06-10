6a3467c9e0fb9c458ced7828a1399f3440ea0f6a
chayuan <gwenael.rob@gmail.com>
2024-06-10 10:11:15 +0200

    feat: add luaSnip

---
+++ nvim/lua/gwenael/plugins/luaSnip.lua
@@ -0,0 +1,11 @@
+return {
+	"L3MON4D3/LuaSnip",
+	dependencies = { "rafamadriz/friendly-snippets" },
+	-- follow latest release.
+	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- install jsregexp (optional!).
+	build = "make install_jsregexp",
+	config = function()
+		local ls = require("luasnip")
+	end,
+}
