b854976e5ba525a810f1580ca6554eb398d27515
chayuan <gwenael.rob@gmail.com>
2024-06-10 19:02:13 +0200

    feat: update nvim theme

--- nvim/lua/gwenael/lazy.lua
+++ nvim/lua/gwenael/lazy.lua
@@ -1,29 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 require("lazy").setup({
-    {import = "gwenael.plugins"},
-    {import = "gwenael.plugins.lsp"}
-  }, {
-    install = {
-      colorscheme = { "nightfly" },
-    },
-    checker = {
-      enabled = true,
-      notify = false,
-    }, 
-    change_detection = {
-        notify = true
-    }
+	{ import = "gwenael.plugins" },
+	{ import = "gwenael.plugins.lsp" },
+}, {
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = true,
+	},
 })
--- nvim/lua/gwenael/plugins/colorscheme.lua
+++ nvim/lua/gwenael/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
 return {
-    "folke/tokyonight.nvim", 
-    priority= 1000, 
-    config = function()
-      -- load the colorscheme here
-      vim.cmd("colorscheme tokyonight")
-    end,
-
+	"folke/tokyonight.nvim",
+	priority = 1000,
+	config = function()
+		-- load the colorscheme here
+		vim.cmd("colorscheme tokyonight")
+	end,
 }
