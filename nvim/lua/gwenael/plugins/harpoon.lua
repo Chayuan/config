6d69977e35cebc90b0f6dd9f3e0ebebfe6d37983
chayuan <gwenael.rob@gmail.com>
2024-06-14 12:15:07 +0200

    🔥 Nvim
    remove unused packages

--- nvim/lua/gwenael/plugins/harpoon.lua
+++ nvim/lua/gwenael/plugins/harpoon.lua
@@ -1,17 +1,15 @@
 return {
-  "ThePrimeagen/harpoon",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function() 
-    local harpoon = require('harpoon')
-    harpoon.setup({})
+	"ThePrimeagen/harpoon",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon.setup({})
 
-    local keymap = vim.keymap
+		local keymap = vim.keymap
 
-    keymap.set("n", "<leader>ha", "<cmd>lua require(\"harpoon.mark\").add_file()<cr>")
-    keymap.set("n", "<leader>hh", "<cmd>lua require(\"harpoon.ui\").toggle_quick_menu()<cr>")
-    keymap.set("n", "<leader>hj", "<cmd>lua require(\"harpoon.ui\").nav_next()<cr>")
-    keymap.set("n", "<leader>hk", "<cmd>lua require(\"harpoon.ui\").nav_prev()<cr>")
-
-
-  end,
+		keymap.set("n", "<leader>ha", '<cmd>lua require("harpoon.mark").add_file()<cr>')
+		keymap.set("n", "<leader>hh", '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>')
+		keymap.set("n", "<leader>hj", '<cmd>lua require("harpoon.ui").nav_next()<cr>')
+		keymap.set("n", "<leader>hk", '<cmd>lua require("harpoon.ui").nav_prev()<cr>')
+	end,
 }
--- nvim/lua/gwenael/plugins/surround.lua
+++
@@ -1,9 +0,0 @@
-return  {
-  "kylechui/nvim-surround",
-  config = function()
-      local surround = require("nvim-surround") 
-      surround.setup({
-      })
-  end,
-}
-
