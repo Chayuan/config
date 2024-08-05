c0a8e5c97746fd5e9b16bf44ef6dc3e710229f5c
chayuan <gwenael.rob@gmail.com>
2024-08-05 09:24:11 +0200

    ♻️ Conform
    Update syntax after plugin update

--- nvim/lua/gwenael/plugins/lsp/conform.lua
+++ nvim/lua/gwenael/plugins/lsp/conform.lua
@@ -8,11 +8,10 @@ return {
 				-- Conform will run multiple formatters sequentially
 				-- Use a sub-list to run only the first available formatter of the sub-list
 				python = { "isort", "black" },
-
-				javascript = { { "prettierd", "prettier" }, "eslint_d" },
-				javascriptreact = { { "prettierd", "prettier" }, "eslint_d" },
-				typescript = { { "prettierd", "prettier" }, "eslint_d" },
-				typescriptreact = { { "prettierd", "prettier" }, "eslint_d" },
+				javascript = { "prettier", "eslint_d" },
+				javascriptreact = { "prettier", "eslint_d" },
+				typescript = { "prettier", "eslint_d" },
+				typescriptreact = { "prettier", "eslint_d" },
 			},
 			format_after_save = {
 				-- These options will be passed to conform.format()
