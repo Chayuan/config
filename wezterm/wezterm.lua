9386b286637aad34265aa2124661e210d642e3b1
chayuan <gwenael.rob@gmail.com>
2024-08-12 10:58:35 +0200

    ✨ (Wezterm): Fix iso layouts

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,12 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
 -- config.color_scheme = 'Batman'
-
-config.font = wezterm.font 'FiraCode Nerd Font'
+config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 16
 
+-- Fix (curly) brackets on non-qwerty keyboards
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+
 return config
-
-
