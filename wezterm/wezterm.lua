05434a3b85d674ae78c6603b357630a93a1adb3c
Chayuan <gwenael.rob@gmail.com>
2024-08-12 10:05:05 +0200

    ✨ (Wezterm): Add wezterm configuration

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+-- config.color_scheme = 'Batman'
+
+config.font = wezterm.font 'FiraCode Nerd Font'
+config.font_size = 16
+
+return config
+
+
