5a46427db5476f2a6314d16d53cd068b9a930135
chayuan <37112020+Chayuan@users.noreply.github.com>
2024-09-13 10:02:34 +0200

    ✨ (Colorscheme): Update colorscheme

--- nvim/lua/gwenael/plugins/colorscheme.lua
+++ nvim/lua/gwenael/plugins/colorscheme.lua
@@ -1,8 +1,30 @@
+-- return {
+-- 	"folke/tokyonight.nvim",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		-- load the colorscheme here
+-- 		vim.cmd("colorscheme tokyonight")
+-- 	end,
+-- }
+
 return {
-	"folke/tokyonight.nvim",
+	"navarasu/onedark.nvim",
 	priority = 1000,
 	config = function()
-		-- load the colorscheme here
-		vim.cmd("colorscheme tokyonight")
+		local theme = require("onedark")
+		theme.setup({
+			style = "darker",
+		})
+		theme.load()
 	end,
 }
+
+-- return {
+-- 	"rose-pine/neovim",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("rose-pine").setup({
+-- 			variant = "dawn", -- auto, main, moon, or dawn
+-- 		})
+-- 	end,
+-- }
